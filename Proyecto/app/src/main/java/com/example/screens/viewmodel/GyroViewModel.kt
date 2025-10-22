b0d0faf3b3cb6c3913fe33dad7b98fab0b4fa891
savaged-22 <juanpabloespinosa2014@gmail.com>
2025-10-22 14:54:11 -0500

    modificacion de sensores

--- Proyecto/app/src/main/java/com/example/screens/viewmodel/GyroViewModel.kt
+++ Proyecto/app/src/main/java/com/example/screens/viewmodel/GyroViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.screens.viewmodel
 
-class GyroViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.screens.sensors.GyroscopeSensor
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class GyroViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _isTiltForward = MutableStateFlow(false)
+    val isTiltForward = _isTiltForward.asStateFlow()
+
+    private val gyroSensor = GyroscopeSensor(application.applicationContext) { tilt ->
+        viewModelScope.launch {
+            _isTiltForward.value = tilt
+        }
+    }
+
+    fun startListening() = gyroSensor.start()
+    fun stopListening() = gyroSensor.stop()
 }
