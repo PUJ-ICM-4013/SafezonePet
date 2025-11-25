50220cd3cf158e6c2ef4a1ee470b9d3cd5628a38
XiomaraHerrera <x.herrerasuarez@gmail.com>
2025-11-25 16:59:09 -0500

    add: ubicaciontiemporeal

---
+++ Proyecto/app/src/main/java/com/example/screens/data/RealtimeLocation.kt
@@ -0,0 +1,8 @@
+package com.example.screens.data
+
+data class RealtimeLocation(
+    val petId: String = "",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val timestamp: Long = 0
+)
